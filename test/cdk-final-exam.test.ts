c5e7e0c6b74a140291d967607a89f474209aaa13
oladayo <akinlonudayo@yahoo.com>
2025-10-15 20:42:51 -0400

    Cdk Final Exam

---
+++ test/cdk-final-exam.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkFinalExam from '../lib/cdk-final-exam-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-final-exam-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkFinalExam.CdkFinalExamStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
